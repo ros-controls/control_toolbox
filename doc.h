908000164cd6fcb8480b2368012c572b8a63b665
Aris Synodinos <arissynod@gmail.com>
2017-11-30 12:03:09 -0500

    Added LeadLag controller

--- doc.h
+++ doc.h
@@ -4,9 +4,11 @@
 
 The control_toolbox package contains the following C++ classes which may be useful when implementing controllers.
  \li \ref control_toolbox::Pid "Pid" for implementing position/integral/derivative control loops.
+ \li \ref control_toolbox::LeadLag "Lead Lag" for implementing lead or lag compensator control loops.
  \li \ref control_toolbox::Ramp "Ramp Output"
  \li \ref control_toolbox::SineSweep "Sine Sweep"
  \li \ref control_toolbox::Dither "Dither" for giving white noise at specified amplitude
  \li \ref control_toolbox::PidGainsSetter "Pid gains setter" for advertising a service for automatically changing PID gains.
+ \li \ref control_toolbox::LeadLagGainsSetter "Lead Lag gains setter" for advertising a service for automatically changing LeadLag gains.
 
 **/

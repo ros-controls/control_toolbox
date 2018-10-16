49fc2a66364f2b441f825db152f06d16a5cbe6ea
Sean Yen <seanyen@microsoft.com>
2018-10-16 00:47:41 -0700

    add missing erand48 for windows.

--- src/dither.cpp
+++ src/dither.cpp
@@ -36,6 +36,17 @@
 
 #include <control_toolbox/dither.h>
 
+#ifdef WIN32
+#include <random>
+#include <iostream>
+std:: default_random_engine generator;
+std::uniform_real_distribution<double> distr(0.0,1.0);
+double erand48(unsigned short xsubi[3])
+{
+  return distr(generator);
+}
+#endif
+
 namespace control_toolbox {
 
 Dither::Dither() : amplitude_(0), has_saved_value_(false)

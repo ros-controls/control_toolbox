a0ae96ebe74375dfb7a90e7c2b292d221c3f10f1
Jordan Palacios <jordan.palacios@pal-robotics.com>
2020-02-18 17:39:58 +0100

    Removed dynamic reconfigure completely

--- include/control_toolbox/pid.h
+++ include/control_toolbox/pid.h
@@ -42,11 +42,6 @@
 
 // #include <control_msgs/PidState.h>
 
-// Dynamic reconfigure
-// #include <dynamic_reconfigure/server.h>
-// #include <control_toolbox/ParametersConfig.h>
-// #include <boost/thread/mutex.hpp>
-
 // Realtime buffer
 #include <realtime_tools/realtime_buffer.h>
 #include <realtime_tools/realtime_publisher.h>
@@ -244,12 +239,6 @@ public:
    */
   // bool initXml(TiXmlElement *config);
 
-  /**
-   * @brief Start the dynamic reconfigure node and load the default values
-   * @param node - a node handle where dynamic reconfigure services will be published
-   */
-  // void initDynamicReconfig(ros::NodeHandle &node);
-
   /*!
    * \brief Reset the state of this PID controller
    */
@@ -287,18 +276,6 @@ public:
    * \param gains A struct of the PID gain values
    */
   void setGains(const Gains & gains);
-
-  /**
-   * @brief Set Dynamic Reconfigure's gains to Pid's values
-   */
-  // void updateDynamicReconfig();
-  // void updateDynamicReconfig(Gains gains_config);
-  // void updateDynamicReconfig(control_toolbox::ParametersConfig config);
-
-  /**
-   * \brief Update the PID parameters from dynamics reconfigure
-   */
-  // void dynamicReconfigCallback(control_toolbox::ParametersConfig &config, uint32_t /*level*/);
 
   /*!
    * \brief Set the PID error and compute the PID command with nonuniform time
@@ -385,14 +362,6 @@ private:
   double i_error_; /**< Integral of position error. */
   double d_error_; /**< Derivative of position error. */
   double cmd_;     /**< Command to send. */
-
-  // Dynamics reconfigure
-  // bool dynamic_reconfig_initialized_;
-  // typedef dynamic_reconfigure::Server<control_toolbox::ParametersConfig> DynamicReconfigServer;
-  // boost::shared_ptr<DynamicReconfigServer> param_reconfig_server_;
-  // DynamicReconfigServer::CallbackType param_reconfig_callback_;
-
-  // boost::recursive_mutex param_reconfig_mutex_;
 
   NodeParamsIfacePtr node_param_iface_;
   

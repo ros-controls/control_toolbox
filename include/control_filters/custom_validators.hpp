93955a84eb354ebba8d7589c0d055a0f193e1982
Christoph Fröhlich <christophfroehlich@users.noreply.github.com>
2024-12-07 09:18:54 +0100

    Fix bug in rate_limiter filter and add more tests (#237)

---
+++ include/control_filters/custom_validators.hpp
@@ -0,0 +1,58 @@
+// Copyright 2024 AIT - Austrian Institute of Technology GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef CONTROL_FILTERS__CUSTOM_VALIDATORS_HPP_
+#define CONTROL_FILTERS__CUSTOM_VALIDATORS_HPP_
+
+#include <fmt/core.h>
+
+#include <string>
+
+#include <rclcpp/rclcpp.hpp>
+#include <rsl/parameter_validators.hpp>
+#include <tl_expected/expected.hpp>
+
+namespace control_filters {
+
+/**
+ * @brief gt_eq, but check only if the value is not NaN
+ */
+template <typename T>
+tl::expected<void, std::string> gt_eq_or_nan(
+    rclcpp::Parameter const& parameter, T expected_value) {
+  auto param_value = parameter.as_double();
+  if (!std::isnan(param_value)) {
+    // check only if the value is not NaN
+    return rsl::gt_eq<T>(parameter, expected_value);
+  }
+  return {};
+}
+
+/**
+ * @brief lt_eq, but check only if the value is not NaN
+ */
+template <typename T>
+tl::expected<void, std::string> lt_eq_or_nan(
+    rclcpp::Parameter const& parameter, T expected_value) {
+  auto param_value = parameter.as_double();
+  if (!std::isnan(param_value)) {
+    // check only if the value is not NaN
+    return rsl::lt_eq<T>(parameter, expected_value);
+  }
+  return {};
+}
+
+}  // namespace control_filters
+
+#endif  // CONTROL_FILTERS__CUSTOM_VALIDATORS_HPP_
--- include/control_toolbox/rate_limiter.hpp
+++ include/control_toolbox/rate_limiter.hpp
@@ -56,14 +56,14 @@ public:
    *
    */
   RateLimiter(
-    T min_value = std::numeric_limits<double>::quiet_NaN(),
-    T max_value = std::numeric_limits<double>::quiet_NaN(),
-    T min_first_derivative_neg = std::numeric_limits<double>::quiet_NaN(),
-    T max_first_derivative_pos = std::numeric_limits<double>::quiet_NaN(),
-    T min_first_derivative_pos = std::numeric_limits<double>::quiet_NaN(),
-    T max_first_derivative_neg = std::numeric_limits<double>::quiet_NaN(),
-    T min_second_derivative = std::numeric_limits<double>::quiet_NaN(),
-    T max_second_derivative = std::numeric_limits<double>::quiet_NaN());
+    T min_value = std::numeric_limits<T>::quiet_NaN(),
+    T max_value = std::numeric_limits<T>::quiet_NaN(),
+    T min_first_derivative_neg = std::numeric_limits<T>::quiet_NaN(),
+    T max_first_derivative_pos = std::numeric_limits<T>::quiet_NaN(),
+    T min_first_derivative_pos = std::numeric_limits<T>::quiet_NaN(),
+    T max_first_derivative_neg = std::numeric_limits<T>::quiet_NaN(),
+    T min_second_derivative = std::numeric_limits<T>::quiet_NaN(),
+    T max_second_derivative = std::numeric_limits<T>::quiet_NaN());
 
   /**
    * \brief Limit the value and first_derivative
@@ -123,14 +123,14 @@ public:
    * If min_first_derivative_pos/max_first_derivative_neg values are NAN, symmetric limits are used
    */
   void set_params(
-    T min_value = std::numeric_limits<double>::quiet_NaN(),
-    T max_value = std::numeric_limits<double>::quiet_NaN(),
-    T min_first_derivative_neg = std::numeric_limits<double>::quiet_NaN(),
-    T max_first_derivative_pos = std::numeric_limits<double>::quiet_NaN(),
-    T min_first_derivative_pos = std::numeric_limits<double>::quiet_NaN(),
-    T max_first_derivative_neg = std::numeric_limits<double>::quiet_NaN(),
-    T min_second_derivative = std::numeric_limits<double>::quiet_NaN(),
-    T max_second_derivative = std::numeric_limits<double>::quiet_NaN());
+    T min_value = std::numeric_limits<T>::quiet_NaN(),
+    T max_value = std::numeric_limits<T>::quiet_NaN(),
+    T min_first_derivative_neg = std::numeric_limits<T>::quiet_NaN(),
+    T max_first_derivative_pos = std::numeric_limits<T>::quiet_NaN(),
+    T min_first_derivative_pos = std::numeric_limits<T>::quiet_NaN(),
+    T max_first_derivative_neg = std::numeric_limits<T>::quiet_NaN(),
+    T min_second_derivative = std::numeric_limits<T>::quiet_NaN(),
+    T max_second_derivative = std::numeric_limits<T>::quiet_NaN());
 
 private:
   // Enable/Disable value/first_derivative/second_derivative limits:
@@ -139,18 +139,18 @@ private:
   bool has_second_derivative_limits_ = true;
 
   // value limits:
-  T min_value_ = std::numeric_limits<double>::quiet_NaN();
-  T max_value_ = std::numeric_limits<double>::quiet_NaN();
+  T min_value_ = std::numeric_limits<T>::quiet_NaN();
+  T max_value_ = std::numeric_limits<T>::quiet_NaN();
 
   // first_derivative limits:
-  T min_first_derivative_neg_ = std::numeric_limits<double>::quiet_NaN();
-  T max_first_derivative_pos_ = std::numeric_limits<double>::quiet_NaN();
-  T min_first_derivative_pos_ = std::numeric_limits<double>::quiet_NaN();
-  T max_first_derivative_neg_ = std::numeric_limits<double>::quiet_NaN();
+  T min_first_derivative_neg_ = std::numeric_limits<T>::quiet_NaN();
+  T max_first_derivative_pos_ = std::numeric_limits<T>::quiet_NaN();
+  T min_first_derivative_pos_ = std::numeric_limits<T>::quiet_NaN();
+  T max_first_derivative_neg_ = std::numeric_limits<T>::quiet_NaN();
 
   // second_derivative limits:
-  T min_second_derivative_ = std::numeric_limits<double>::quiet_NaN();
-  T max_second_derivative_ = std::numeric_limits<double>::quiet_NaN();
+  T min_second_derivative_ = std::numeric_limits<T>::quiet_NaN();
+  T max_second_derivative_ = std::numeric_limits<T>::quiet_NaN();
 };
 
 template <typename T>
